31f95de9d5b010870596f364ba660291dcdbab41
vgvishesh <vgvishesh2008@gmail.com>
2016-06-01 16:24:39 +0530

    completing the basic node js api, to interact with the frontend

--- server.js
+++ server.js
@@ -14,6 +14,57 @@ app.use(bodyParser.json());                                     // parse applica
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
 
+
+// defining the model
+var toDo = mongoose.model('ToDo', {
+	text : String
+})
+
+
+// defining the routes
+ app.get('/api/todos', function (req, res){
+ 	toDo.find(function (err, todos)
+ 	{
+ 		if(err)
+ 			res.send(err);
+
+ 		res.json(todos);
+ 	})
+ })
+
+app.post('/api/todos', function (req, res) {
+	toDo.Create({
+		text : req.body.text,
+		done : false
+	}, function(err, todo) {
+		if(err)
+			res.send(err);
+
+		toDo.find(function (err, todos)
+		{
+			if(err)
+				res.send(err);
+			res.json(todos);
+		})
+	})
+})
+
+app.delete('/api/todos/:todo_id', function (req, res) {
+	toDo.remove({
+		_id : req.params.todo_id
+	}, function (err, todo) {
+		if(err)
+			res.send(err);
+
+		toDo.find(function (err, todos) {
+			if(err)
+				res.send(err);
+			res.json(todos);
+		})
+	})
+})
+
+
 // listen (start app with node server.js) ======================================
 var server = app.listen(8080, function() {
 	var host = server.address().address;
